370ae6978aa28d72262ceeddc66a8b056c3f0256
Nell Fauveau <nellousan@hotmail.fr>
2021-11-14 19:03:15 +0100

    feat(body): Added body follow functionnality.

--- scenes/Snake/Body/Body.cs
+++ scenes/Snake/Body/Body.cs
@@ -3,27 +3,47 @@ using System.Collections.Generic;
 
 public class Body : Node2D
 {
-    public class Transition 
+    public class Transition
     {
         Transition(Vector2 position_, float angle_)
         {
             position = position_;
             angle = angle_;
         }
-        Vector2 position;
-        float angle;
+        public Vector2 position;
+        public float angle;
     }
 
     public List<Transition> Transitions = new List<Transition>();
     private Tween _tween;
+    private Timer _timer;
+    public Body NextBody;
 
     public override void _Ready()
     {
         _tween = GetNode<Tween>("Tween");
+        _timer = GetNode<Timer>("Timer");
+        _timer.OneShot = false;
+        _timer.Start(0.1f);
+        _timer.Connect("timeout", this, nameof(_onTimerTimeout));
     }
 
     public override void _Process(float delta)
     {
-        
+
+    }
+
+    private void _onTimerTimeout()
+    {
+        if (Transitions.Count >= 10)
+        {
+            Transition transition = Transitions[0];
+            _tween.InterpolateProperty(this, "position", Position, transition.position, 0.1f);
+            _tween.InterpolateProperty(this, "rotation_degrees", RotationDegrees, transition.angle, 0.1f);
+            _tween.Start();
+            if (NextBody != null) {
+                NextBody.Transitions.Add(transition);
+            }
+        }
     }
 }
