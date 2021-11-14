92520cb58696b306ad52ab4ad7ca1290520c67af
Nell Fauveau <nellousan@hotmail.fr>
2021-11-14 18:32:47 +0100

    feat(body): added transitions

--- scenes/Snake/Body/Body.cs
+++ scenes/Snake/Body/Body.cs
@@ -1,21 +1,29 @@
 using Godot;
-using System;
+using System.Collections.Generic;
 
 public class Body : Node2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    public class Transition 
+    {
+        Transition(Vector2 position_, float angle_)
+        {
+            position = position_;
+            angle = angle_;
+        }
+        Vector2 position;
+        float angle;
+    }
 
-    // Called when the node enters the scene tree for the first time.
+    public List<Transition> Transitions = new List<Transition>();
+    private Tween _tween;
+
     public override void _Ready()
+    {
+        _tween = GetNode<Tween>("Tween");
+    }
+
+    public override void _Process(float delta)
     {
         
     }
-
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
 }
