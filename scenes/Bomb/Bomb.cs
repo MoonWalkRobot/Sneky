e958ff9c8528fa078db4d916d2ba8524743f7d11
Nell Fauveau <nellousan@hotmail.fr>
2021-11-24 16:01:54 +0100

    feat(gameplay): added bombs

---
+++ scenes/Bomb/Bomb.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public class Bomb : Node2D
+{
+    [Signal] public delegate void Dead();
+
+    public override void _Ready()
+    {
+        
+    }
+
+    public void Die() {
+        EmitSignal(nameof(Dead));
+        QueueFree();
+    }
+}
