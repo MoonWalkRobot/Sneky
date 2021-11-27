d3a375b99daa958b486bfabf385d55a0ce23efe4
Bryan <bryan.letohic@outlook.fr>
2021-11-27 01:08:49 +0100

    feat(Bomb/Food): Added idles

--- scenes/Bomb/Bomb.cs
+++ scenes/Bomb/Bomb.cs
@@ -4,10 +4,12 @@ using System;
 public class Bomb : Node2D
 {
     [Signal] public delegate void Dead();
+    private AnimatedSprite animatedSprite;
 
     public override void _Ready()
     {
-        
+        animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+        animatedSprite.Play("Apparition");
     }
 
     public void Die() {
--- scenes/Food/Food.cs
+++ scenes/Food/Food.cs
@@ -4,13 +4,26 @@ using System;
 public class Food : Node2D
 {
     [Signal] public delegate void Dead();
+    private AnimatedSprite animatedSprite;
+    Random rnd = new Random();
+
+    private const int FamilySize = 4;
+    enum Octopus {
+        basic,
+        shiny,
+        makeup,
+        old
+    }
+
 
     public override void _Ready()
     {
-        
+        animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
+        animatedSprite.Play("idle_" + (Octopus) rnd.Next(FamilySize));
     }
 
-    public void Die() {
+    public void Die()
+    {
         EmitSignal(nameof(Dead));
         QueueFree();
     }
