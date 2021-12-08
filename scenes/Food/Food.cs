4de12f6a1259a08f60ae2fd973d9fb83f006468d
Bryan <bryan.letohic@outlook.fr>
2021-12-08 18:45:47 +0100

    feat(Food): Added rarity

--- scenes/Food/Food.cs
+++ scenes/Food/Food.cs
@@ -6,9 +6,9 @@ public class Food : Node2D
     public enum Octopus
     {
         basic,
-        shiny,
         makeup,
-        old
+        old,
+        shiny
     }
     [Signal] public delegate void Dead(bool alt);
     private AnimatedSprite animatedSprite;
@@ -17,9 +17,32 @@ public class Food : Node2D
 
     public override void _Ready()
     {
-        Type = (Octopus)rnd.Next(Enum.GetNames(typeof(Octopus)).Length);
+        Type = (Octopus)SelectOctopus();
         animatedSprite = GetNode<AnimatedSprite>("AnimatedSprite");
         animatedSprite.Play("idle_" + Type);
+    }
+
+    private int SelectOctopus()
+    {
+        int num = rnd.Next(100);
+        GD.Print(num);
+        if (num >= 90)
+        {
+            return Enum.GetNames(typeof(Octopus)).Length - 1;
+        }
+        else if (num >= 60)
+        {
+            return Enum.GetNames(typeof(Octopus)).Length - 2;
+        }
+        else if (num >= 30)
+        {
+            return Enum.GetNames(typeof(Octopus)).Length - 3;
+        }
+        else
+        {
+            return Enum.GetNames(typeof(Octopus)).Length - 4;
+        }
+
     }
 
     public void Die()
